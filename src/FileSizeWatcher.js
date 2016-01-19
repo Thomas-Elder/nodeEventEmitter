e1a424b5832a66c3d9a559308c03f40ab34a9869
Thomas Elder <thomas.charles.elder@gmail.com>
2016-01-19 14:30:03 +1100

    Corrected jshint errors and warning for node code
    jshint was reporting errors for things like require, and 'use strict', so
    I've created .jshintrc with the node option set true.
    
    This fixed spurious errors/warnings, but also highlighted a couple others.
    Like using ['text'](); to call a member of an object was highlighted, should
    be using dot notation, like object.text();
    
    I'm sure it's a matter of style, but I like the dot notation better too.

--- src/FileSizeWatcher.js
+++ src/FileSizeWatcher.js
@@ -12,7 +12,7 @@ var FileSizeWatcher = function(path){
   // Error if file has incorrect extension.
   if(/^.+\.test/.test(path) === false) {
     process.nextTick(function(){
-      self.callbacks['error']('Path is not of extenstion ".test".');
+      self.callbacks.error('Path is not of extenstion ".test".');
     });
 
     return;
@@ -31,12 +31,12 @@ var FileSizeWatcher = function(path){
       fs.stat(path, function(err, stats){
 
         if(stats.size < self.lastFileSize){
-          self.callbacks['shrank'](self.lastFileSize - stats.size);
+          self.callbacks.shrank(self.lastFileSize - stats.size);
           self.lastFileSize = stats.size;
         }
 
         if(stats.size > self.lastFileSize){
-          self.callbacks['grew'](stats.size - self.lastFileSize);
+          self.callbacks.grew(stats.size - self.lastFileSize);
           self.lastFileSize = stats.size;
         }
     });
